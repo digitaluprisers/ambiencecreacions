12ee22be27c80605f3ed0c6e9e55befb56483e2d
Shahnawaz <digitaluprisers@gmail.com>
2025-07-19 15:55:16 +0530

    Fix: remove merge conflict markers from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
-<<<<<<< HEAD
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 import TermsConditionsPage from './pages/TermsConditionsPage';
 import ThankYou from './pages/ThankYou';
-=======
->>>>>>> 92038dcce275492eec33835472d3ff77df0584b4
 import './App.css';
 
 const queryClient = new QueryClient();
@@ -21,12 +18,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-<<<<<<< HEAD
           <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
           <Route path="/terms-conditions" element={<TermsConditionsPage />} />
           <Route path="/thank-you" element={<ThankYou />} />
-=======
->>>>>>> 92038dcce275492eec33835472d3ff77df0584b4
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
